e74c23e586e126761a634b0f5431acf656559aea
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2026-08-30 17:50:12 -0300

    i18n(lang): [ADD] adicionar traduções nl, es, fr e zh_cn
    Motivo: completar a internacionalização do plugin, atendendo às
    issues de tradução já abertas (#33 holandês, #32 espanhol, #31
    francês, #29 chinês simplificado), espelhando as 15 chaves já
    existentes em lang/pt_br/tool_painelava.php e lang/en/tool_painelava.php
    (paridade confirmada).
    
    Impacto: usuários com Moodle configurado em nl, es, fr ou zh_cn passam
    a ver as strings do plugin traduzidas em vez de cair no fallback en.
    Todos os 6 idiomas agora têm exatamente 15 chaves (validado com
    grep). Sintaxe PHP validada com `php -l` em todos os arquivos novos.
    
    Arquivos: lang/nl/tool_painelava.php, lang/es/tool_painelava.php,
    lang/fr/tool_painelava.php, lang/zh_cn/tool_painelava.php (novos);
    version.php incrementado para 4.5.020.
    
    Closes #33, Closes #32, Closes #31, Closes #29

---
+++ lang/nl/tool_painelava.php
@@ -0,0 +1,41 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Nederlandse taalstrings voor tool_painelava.
+ *
+ * @package    tool_painelava
+ * @copyright  2024 IFRN
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string["auth_token"] = 'Authenticatietoken';
+$string["auth_token_desc"] = 'Het token dat door Painel AVA wordt gebruikt om zich te authenticeren bij deze Moodle-installatie.';
+$string['auth_token_header'] = 'Painel AVA';
+$string['auth_token_header_desc'] = 'Het token dat door Painel AVA wordt gebruikt om zich te authenticeren bij deze Moodle-installatie.';
+$string['course_custom_field_sala_tipo'] = 'Aangepast cursusveld: Sala Tipo';
+$string['course_custom_field_sala_tipo_desc'] = 'Dit aangepaste cursusveld wordt gebruikt om het type ruimte te identificeren dat aan de cursus is gekoppeld, zoals "Laboratorium", "Klaslokaal", "Virtuele ruimte", enz. Het is essentieel voor het categoriseren van cursussen op basis van de leeromgeving en kan worden gebruikt om cursussen in het Painel AVA te filteren of te organiseren.';
+$string['event_user_courses_requested'] = 'Cursuslijst van gebruiker opgevraagd';
+$string['invaliduser'] = 'De opgegeven gebruiker bestaat niet of is verwijderd.';
+$string['painel_url'] = 'Painel AVA-URL';
+$string['painel_url_desc'] = 'Wordt gebruikt om de gebruikersvoorkeuren bij te werken (bijv. https://ava.ifrn.edu.br)';
+$string['painelava:view'] = 'Het Painel AVA-paneel bekijken';
+$string['painelava:viewothercourses'] = 'De cursuslijst van een andere gebruiker bekijken';
+$string['pluginname'] = 'Painel AVA';
+$string['pluginname_desc'] = 'Painel AVA is een beheertool die een externe API biedt om cursusgegevens op te halen, gegroepeerd per type, inclusief aangepaste velden en gebruikersrollen.';
+$string['task_sync_courses'] = 'Painel AVA – cursusgegevens synchroniseren';
--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_painelava';
-$plugin->release   = '4.5.019';
-$plugin->version   = 2026_08_30_019;
+$plugin->release   = '4.5.020';
+$plugin->version   = 2026_08_30_020;
 $plugin->requires  = 2024100710; // Moodle 4.5 or later.
 $plugin->maturity  = MATURITY_STABLE;
